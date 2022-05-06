b6282d1149ef00f35093edd211c7b9e1d60f27de
Ben Neale <ben0neale@gmail.com>
2022-05-06 17:00:40 +0100

    Menu And Buttons

---
+++ Soul Demo/Assets/ButtonController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonController : MonoBehaviour
+{
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
